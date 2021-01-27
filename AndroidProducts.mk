ff5613108cdbf198de27eb89bc3d212b4fc979a6
SparXFusion <s2234nadar@gmail.com>
2021-01-27 18:07:54 +0530

    RMX1831: adapt to FusionOS
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1831.mk
+    $(LOCAL_DIR)/fuse_RMX1831.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1831-user \
-    lineage_RMX1831-userdebug \
-    lineage_RMX1831-eng
+    fuse_RMX1831-user \
+    fuse_RMX1831-userdebug \
+    fuse_RMX1831-eng
 
